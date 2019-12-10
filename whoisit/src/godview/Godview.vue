818e23ec37b84e6aaef5adfd25e27c410ba835c6
dpkn <9087597+dpkn@users.noreply.github.com>
2019-12-10 12:35:22 +0100

    fixed overflow issue

--- whoisit/src/godview/Godview.vue
+++ whoisit/src/godview/Godview.vue
@@ -85,7 +85,6 @@ export default {
 html, body, #app {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   width: 100%;
-  height: 100%;
   margin: 0;
   padding: 0;
   top:0;
